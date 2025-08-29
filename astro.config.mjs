2231ee85fd791a82d5ec621b9ee3bcc95fa45ac7
Sulhi <22936450+sulhicmz@users.noreply.github.com>
2025-08-29 18:52:35 +0700

    feat: update site URL to maskom.co.id for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ultimate-astro-template.vercel.app/",
+  site: "https://maskom.co.id/",
   output: "static",
   integrations: [
     tailwind({
