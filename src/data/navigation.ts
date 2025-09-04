95ac446aeef7fee1cfe4d378dc3bb32888ef7ce5
Sulhi <22936450+sulhicmz@users.noreply.github.com>
2025-09-04 07:59:28 +0700

    feat: modern responsive layout

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -6,9 +6,6 @@ export interface Framework {
 }
 
 export const frameworks: Framework[] = [
-    { name: 'astro', displayName: 'Astro', path: '/', color: '#FF5D01' },
-    { name: 'react', displayName: 'React', path: '/react', color: '#61DAFB' },
-    { name: 'vuejs', displayName: 'Vue', path: '/vue', color: '#4FC08D' },
-    { name: 'svelte', displayName: 'Svelte', path: '/svelte', color: '#FF3E00' },
-    { name: 'solidjs', displayName: 'Solid', path: '/solid', color: '#2C4F7C' },
+    { name: 'home', displayName: 'Home', path: '/', color: '#FF5D01' },
+    { name: 'paket', displayName: 'Paket', path: '/#paket', color: '#61DAFB' },
 ];
