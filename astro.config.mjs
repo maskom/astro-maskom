1ef0527458fbf18457c5953c7f9a84f55ea5c61b
Sulhi <22936450+sulhicmz@users.noreply.github.com>
2025-11-01 13:20:37 +0700

    Restyle hero and packages with neon glow design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import react from "@astrojs/react";
-import svelte from "@astrojs/svelte";
-import vue from "@astrojs/vue";
-import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 
 // Astro configuration
@@ -17,28 +13,10 @@ export default defineConfig({
       applyBaseStyles: true,
     }),
     icon(),
-    react({
-      include: ["**/React/**/*.{jsx,tsx}"],
-    }),
-    svelte(),
-    vue(),
-    solidJs({
-      include: ["**/Solid/**/*.{jsx,tsx}"],
-    }),
     sitemap(),
   ],
 
   build: {
     inlineStylesheets: "auto",
-  },
-
-  vite: {
-    optimizeDeps: {
-      include: ["react", "react-dom", "solid-js"],
-      exclude: ["@astrojs/solid-js/client.js"],
-    },
-    ssr: {
-      noExternal: ["@astrojs/*"],
-    },
   },
 });
